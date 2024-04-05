a8c0902a53cea8cee3286e215375e0ec39c7de34
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-04 21:55:09 -0300

    debochando

--- src/rodrigocitadin/learnclj.clj
+++ src/rodrigocitadin/learnclj.clj
@@ -1,12 +1,16 @@
 (ns rodrigocitadin.learnclj
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+; (defn greet
+;   "Callable entry point to the application."
+;   [data]
+;   (println (str "Hello, " (or (:name data) "World") "!")))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+; (defn -main
+;   "I don't do a whole lot ... yet."
+;   [& args]
+;   (greet {:name (first args)}))
+
+(defn -main [world]
+  (println (str "Hello, " world)))
+
