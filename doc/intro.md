296492887b06a1a3e5a707661d60352d4c6cb3c6
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-04 21:44:01 -0300

    new

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rodrigocitadin/learnclj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rodrigocitadin/learnclj_test.clj
@@ -0,0 +1,7 @@
+(ns rodrigocitadin.learnclj-test
+  (:require [clojure.test :refer :all]
+            [rodrigocitadin.learnclj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
